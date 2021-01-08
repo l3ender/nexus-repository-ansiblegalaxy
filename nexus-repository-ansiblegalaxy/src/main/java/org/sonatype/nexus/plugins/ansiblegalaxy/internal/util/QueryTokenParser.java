eee80d402c6da1158acf5fdc2f89a653791b7204
Ross Bender <l3ender@users.noreply.github.com>
2021-01-07 22:11:37 -0600

    minor doc updates

--- nexus-repository-ansiblegalaxy/src/main/java/org/sonatype/nexus/plugins/ansiblegalaxy/internal/util/QueryTokenParser.java
+++ nexus-repository-ansiblegalaxy/src/main/java/org/sonatype/nexus/plugins/ansiblegalaxy/internal/util/QueryTokenParser.java
@@ -13,6 +13,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * Extends the {@link TokenParser} to support URI query parameters.
  * 
  * Query parameters are optional. If they exist in the request, tokens from them will be loaded.
+ * 
  * Query parameters are not order specific.
  */
 public class QueryTokenParser
@@ -36,9 +37,13 @@ public class QueryTokenParser
   /**
    * Attempts to parse the provided path against the template pattern, and the query parameters against the query
    * parameter tokens.
-   * If the pattern matches and, the resulting Map contains an entry for each variable in the pattern. The variable
-   * names are keys, with the matching portions of the
-   * path as values. Returns {@code null} if the pattern does not match.
+   * 
+   * If the pattern matches, tokens are loaded from the URL path and query parameter map. For the parameter map:
+   * key represent query parameter names, and values represent the token name in which the query value will be stored.
+   * 
+   * The resulting Map contains an entry for each token extracted from the URL path and query parameters.
+   * 
+   * Returns {@code null} if the pattern does not match.
    */
   @Nullable
   public Map<String, String> parse(final String path, Map<String, String> queryParameters) {
